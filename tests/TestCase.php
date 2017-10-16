2cc7b801c9d4d9150820376230d769ebe344865e
Stefan Graupner <stefan.graupner@gmail.com>
2017-10-16 18:12:44 +0200

    Setup project

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @copyright 2017
+ * @author Stefan "eFrane" Graupner <stefan.graupner@gmail.com>
+ * @license GPL
+ */
+
+namespace Tests;
+
+/**
+ * Base test class for PHPGObject tests
+ */
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+}
